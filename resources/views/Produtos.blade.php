a94a97e717babeed2af37cb7fb58ab2fec82d220
Jackstiven <fe.oliveira.camilo@gmail.com>
2025-03-24 22:10:11 -0300

    A

---
+++ resources/views/Produtos.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <h1>Produtos</h1>
+
+    <a href="{{ route('Site.Home')}}">Home</a>
+    <a href="{{ route('Site.Localizacao')}}">Localizacao</a>
+    <a href="{{ route('Site.Produtos')}}">Produtos</a>
+    <a href="{{ route('Site.SobreNos')}}">SobreNos</a>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [\App\Http\Controllers\Home::Class, 'Home']);
+Route::get('/', [\App\Http\Controllers\Home::Class, 'Home'])->name('Site.Principal');
 
 
-Route::get('/Localizacao', [\App\Http\Controllers\Localizacao::class, 'Localizacao']);
+Route::get('/Localizacao', [\App\Http\Controllers\Localizacao::class, 'Localizacao'])->name('Site.Localizacao');
 
 
-Route::get('/Produtos', [\App\Http\Controllers\Produtos::class, 'Produtos']);
+Route::get('/Produtos', [\App\Http\Controllers\Produtos::class, 'Produtos'])->name('Site.Produtos');
 
 
-Route::get('/SobreNos', [\App\Http\Controllers\SobreNos::class, 'SobreNos']);
+Route::get('/SobreNos', [\App\Http\Controllers\SobreNos::class, 'SobreNos'])->name('Site.SobreNos');
